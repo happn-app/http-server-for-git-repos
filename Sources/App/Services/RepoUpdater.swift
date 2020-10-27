cd90d07451e3f6435face4c2fe20b5d4ac3e6acb
Frizlab <fload@me.com>
2020-10-27 18:44:39 +0100

    Do not allow two git operation on same URL at the same time

--- Sources/App/Services/RepoUpdater.swift
+++ Sources/App/Services/RepoUpdater.swift
@@ -22,17 +22,44 @@ final class RepoUpdater {
 				guard let t = app.config.pullInterval else {
 					return
 				}
-				self.q.asyncAfter(deadline: .now() + .milliseconds(Int(t * 1000)), execute: {
+				self.timeQ.asyncAfter(deadline: .now() + .milliseconds(Int(t * 1000)), execute: {
 					_ = self.startScheduledUpdates(app)
 				})
 			}
 	}
 	
 	func updateAllRepos(_ app: Application) -> EventLoopFuture<Void> {
-		return EventLoopFuture.reduce((), app.config.repos.map{ repoCloner.cloneOrUpdateRepo($0, app: app) }, on: app.eventLoopGroup.next(), { _,_ in })
+		let eventLoop = app.eventLoopGroup.next()
+		return EventLoopFuture.reduce((), app.config.repos.map{ updateRepo($0, app: app, eventLoop: eventLoop) }, on: eventLoop, { _,_ in })
 	}
 	
-	private let q = DispatchQueue(label: "com.happn.http-server-for-git-repos.RepoUpdaterQueue")
+	func updateRepo(_ repo: ServerConfig.Repo, app: Application, req: Request? = nil, eventLoop: EventLoop) -> EventLoopFuture<Void> {
+		return syncQ.sync{
+			let localRepoURL = app.config.localURL(for: repo)
+			let r: EventLoopFuture<Void>
+			if let f = futureUpdateByRepoURL[localRepoURL] {
+				r = f.hop(to: eventLoop).flatMapAlways{ _ in
+					return self.repoCloner.cloneOrUpdateRepo(repo, app: app, req: req).hop(to: eventLoop)
+				}
+			} else {
+				r = repoCloner.cloneOrUpdateRepo(repo, app: app, req: req).hop(to: eventLoop)
+			}
+			futureUpdateByRepoURL[localRepoURL] = r
+			_ = r.always{ _ in
+				self.syncQ.sync{
+					if self.futureUpdateByRepoURL[localRepoURL] === r {
+						self.futureUpdateByRepoURL.removeValue(forKey: localRepoURL)
+					}
+				}
+			}
+			return r
+		}
+	}
+	
+	private let syncQ = DispatchQueue(label: "com.happn.http-server-for-git-repos.RepoUpdaterQueueForSync")
+	private let timeQ = DispatchQueue(label: "com.happn.http-server-for-git-repos.RepoUpdaterQueueForTime")
 	private let repoCloner = RepoCloner()
+	
+	private var futureUpdateByRepoURL = [URL: EventLoopFuture<Void>]()
 	
 }
