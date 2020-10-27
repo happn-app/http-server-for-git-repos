f4fbd8de2f2941afef9f6475b32d913277a24251
Frizlab <fload@me.com>
2020-10-27 17:25:02 +0100

    Add underscore to the healthz endpoint

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,6 @@ import Vapor
 
 
 func routes(_ app: Application) throws {
-	app.get("healthz", "live",  use: { _   in "ok" })
-	app.get("healthz", "ready", use: { req in req.application.firstCloneDone ? Response(body: "ok") : Response(status: .serviceUnavailable) })
+	app.get("_healthz", "live",  use: { _   in "ok" })
+	app.get("_healthz", "ready", use: { req in req.application.firstCloneDone ? Response(body: "ok") : Response(status: .serviceUnavailable) })
 }
